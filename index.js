e74c030f198bc9fb939bfff26002cb909d260d86
Maverick Tesini <mavericktesini@gmail.com>
2018-08-14 19:08:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let trip = {drivers: [], passengers: []};
+let store = {drivers: [], passengers: []};
 let driverId = 0;
 let passengerId = 0;
 
@@ -7,17 +7,14 @@ class Driver {
   constructor(name) {
     this.name = name;
     this.id = ++driverId;
-    trip.drivers.push(this);
+    store.drivers.push(this);
   }
 
   trips() {
-    return trip.drivers.filter(driver => {
-      return driver.id === this.id;
-    })
   }
 
   passengers() {
-    return trip.passengers.filter(passenger => {
+    return store.passengers.filter(passenger => {
       return passenger.driverId === this.id;
     })
   }
