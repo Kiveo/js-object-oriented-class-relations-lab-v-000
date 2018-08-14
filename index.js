468a094670d38422da2ecaeb2b949040c3cbb427
Maverick Tesini <mavericktesini@gmail.com>
2018-08-14 18:53:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 let store = {drivers: [], passengers: []};
-
 let driver.id = 0;
+let passenger.id = 0;
+
 class Driver {
   constructor(name) {
     this.name = name;
     this.id = ++driver.id;
-  }
-  trips() {
     trips.drivers.push(this);
   }
+  trips() {
+
+  }
 } //EOC
+
+class Passenger() {
+
+  constructor(name) {
+    this.name = name;
+    this.id = ++passenger.id;
+
+
+  }
+}
