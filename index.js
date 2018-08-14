ff2e47f9c5bb7ea8412e6ed5e4f0b6290769cad6
Maverick Tesini <mavericktesini@gmail.com>
2018-08-14 19:34:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,9 +11,7 @@ class Driver {
   }
 
   trips() {
-    return store.trips.filter(trip => {
-      trip.driverId === this.id;
-    });
+    return store.trips.filter(trip => trip.driverId === this.id);
   }
 
   passengers() {
